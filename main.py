5c5baf92ec8f95ad275244493644e60d34666875
Jhon Fredy Beltrán León <jbeltranleon@localhost.localdomain>
2017-08-24 17:55:07 -0500

    Agrego template usando render_template de Flask

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-	return 'Hola Mundo'
+	return render_template('index.html')
 
 
 if __name__ == '__main__':
