f1c1ddfdcf8da853dc4eba18e898e42d2c4d1d35
Jhon Fredy Beltrán León <jbeltranleon@localhost.localdomain>
2017-08-25 18:41:29 -0500

    Agrego ejemplo de ruta con la pagina add

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route(r'/', methods=['GET'])
+def contact_book():
 	return render_template('contact_book.html')
 
+@app.route(r'/add', methods=['GET'])
+def add_contact():
+    return render_template('add_contact.html')
 
 if __name__ == '__main__':
 	app.run()
