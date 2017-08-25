a2176c0178d770d9d3eba34ad663acce2e63cf56
Jhon Fredy Beltrán León <jbeltranleon@localhost.localdomain>
2017-08-25 18:55:18 -0500

    Agrego prints al llenar el formulario en la funcion add_contact

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route(r'/', methods=['GET'])
 def contact_book():
-	return render_template('contact_book.html')
+    return render_template('contact_book.html')
 
-@app.route(r'/add', methods=['GET'])
+@app.route(r'/add', methods=['GET', 'POST'])
 def add_contact():
+
+    if request.form:
+        print(request.form.get('name'))
+        print(request.form.get('phone'))
+        print(request.form.get('email'))
+    
     return render_template('add_contact.html')
 
 if __name__ == '__main__':
-	app.run()
+    app.run()
